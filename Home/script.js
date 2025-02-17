a459842a9d071dad5c3b7a71a655421b6c22925b
Gamlio <thanh097359@gmail.com>
2025-02-18 05:52:54 +0700

    Cosmetics

--- Home/script.js
+++ Home/script.js
@@ -12,5 +12,17 @@ facebookLink.addEventListener('click', (event) => {
 
   window.open('https://www.facebook.com/AlYT.XD', '_blank');
 });
+//skincare
+const steps = document.querySelectorAll('.step');
+const contents = document.querySelectorAll('.content > div');
 
+steps.forEach((step, index) => {
+  step.addEventListener('click', () => {
+    // Ẩn tất cả nội dung
+    contents.forEach(content => content.classList.remove('active'));
+
+    // Hiển thị nội dung tương ứng với bước được click
+    contents[index].classList.add('active');
+  });
+});
 
