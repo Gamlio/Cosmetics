35e7ea7bac10fb90e196335c02f43df9aba4fc65
Gamlio <thanh097359@gmail.com>
2025-02-18 14:21:19 +0700

    Cometics

--- Home/Chatbot/Chatbot.js
+++ Home/Chatbot/Chatbot.js
@@ -12,3 +12,15 @@ facebookLink.addEventListener('click', (event) => {
 
   window.open('https://www.facebook.com/AlYT.XD', '_blank');
 });
+//chat bot
+const messageInput = document.querySelector(".message-input");
+const handleOutgoingMessage=(UserMessage)=>{
+
+}
+
+messageInput.addEventListener("Keydown",(e)=> {
+  const UserMessage = e.target.value.trim();
+  if(e.key==="Enter" && UserMessage){
+    console.log(UserMessage)
+  }
+})
